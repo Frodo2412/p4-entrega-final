b0e41fcfa6e9a09d4f31951a9c4bfe18dd16a4e3
MontoroA <andresmontoro01@gmail.com>
2022-06-06 20:19:34 -0300

    Declaracion enums DtCargo y DtEstado

---
+++ datatypes/DtCargo.h
@@ -0,0 +1,14 @@
+//
+// Created by User on 6/6/2022.
+//
+
+#ifndef P4_ENTREGA_FINAL_DTCARGO_H
+#define P4_ENTREGA_FINAL_DTCARGO_H
+
+enum DtCargo {
+    Abierta;
+    Cerrada;
+    Cancelada
+};
+
+#endif //P4_ENTREGA_FINAL_DTCARGO_H
---
+++ datatypes/DtEstado.h
@@ -0,0 +1,15 @@
+//
+// Created by User on 6/6/2022.
+//
+
+#ifndef P4_ENTREGA_FINAL_DTESTADO_H
+#define P4_ENTREGA_FINAL_DTESTADO_H
+
+enum DtEstado {
+    Administracion;
+    Limpieza;
+    Recepcion;
+    Infraestructura
+};
+
+#endif //P4_ENTREGA_FINAL_DTESTADO_H
