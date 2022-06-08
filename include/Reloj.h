1a171ef62b75be0ff5310e0fc774a7ab86769646
UnZip <dylansmyth206@gmail.com>
2022-06-08 17:57:39 -0300

    Reloj pronto

--- include/Reloj.h
+++ include/Reloj.h
@@ -6,17 +6,17 @@
 #define P4_ENTREGA_FINAL_RELOJ_H
 
 
+#include "DtFecha.h"
+
 class Reloj {
 private:
-    DTfecha fechaActual;
-    Reloj instancia = nullptr;
+    DtFecha fechaActual = DtFecha(1,1,1,2000);
+    static Reloj * instancia;
     Reloj();
 public:
-    Reloj getInstance();
+    static Reloj getInstance();
     DtFecha getFechaActual();
     void modificarFechaDeSistema(int, int, int, int);
-};
-
 };
 
 
--- src/Reloj.cpp
+++ src/Reloj.cpp
@@ -3,24 +3,23 @@
 //
 
 #include "Reloj.h"
-//TODO: Modificar para hacer un DTFecha con valores iniciales
-Reloj::Reloj() {
-    this->fechaActual = DTFecha();
-}
 
-}
+Reloj * Reloj::instancia = nullptr;
+
+Reloj::Reloj() = default;
+
 
 DtFecha Reloj::getFechaActual() {
     return this->fechaActual;
 }
 
 void Reloj::modificarFechaDeSistema(int anio, int mes, int dia, int hora) {
-    this->fechaActual = DTFecha(anio, mes, dia, hora);
+    this->fechaActual = DtFecha(anio, mes, dia, hora);
 }
 
 Reloj Reloj::getInstance() {
     if (instancia == nullptr) {
         instancia = new Reloj();
     }
-    return instancia;
+    return *instancia;
 }
